b93c5b54a8c7202e273cf1cd358bace4c165f456
Hugo <Hugoramand@gmail.com>
2024-08-06 23:43:02 +0200

    add option

--- macFlipper.py
+++ macFlipper.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 import subprocess
+import optparse
 
-interface = input("Saisre une interface : ")
-NewMacAddres=input("Saisire une nouvelle addresse MAC : ")
+def getArguments():
+    parser = optparse.OptionParser()
+    parser.add("-i","--interface",dest="interface",help="Interface to changer MAC address")
+    parser.add("-m","--mac",dest="newMacAddress" ,help="New MAC Address")
+    (option, arguments) = parser.parse_args()
+    if not parser.interface:
+        parser.error("Please specify interface, use --help for more information")
+    elif not parser.newMacAddress:
+            parser.error("Please specify MAC address, use --help for more information")
+    return option
 
 def SetNewAddressMac(interface, NewMacAddres):
     print("Changing MAC address for " + interface + " to " + NewMacAddres)
@@ -11,4 +20,5 @@ def SetNewAddressMac(interface, NewMacAddres):
     subprocess.call("ifconfig " + interface + " hw ether " +  NewMacAddres, shell=True)
     subprocess.call("ifconfig " + interface + " up", shell=True)
 
-SetNewAddressMac(interface,NewMacAddres)
+option = getArguments()
+SetNewAddressMac(option.interface,option.newMacAddress)
