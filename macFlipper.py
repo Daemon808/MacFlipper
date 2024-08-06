bc5ec42af167410933dad296f4e8d4abd5af362a
Daemon <90561166+Daemon808@users.noreply.github.com>
2024-08-06 23:06:05 +0200

    add function + input

--- macFlipper.py
+++ macFlipper.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+
 import subprocess
 
-subprocess.call("ifconfig en0 down", shell=True)
-subprocess.call("ifconfig en0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig en0 up", shell=True)
+interface = input("Saisre une interface : ")
+NewMacAddres=input("Saisire une nouvelle addresse MAC : ")
+
+def SetNewAddressMac(interface, NewMacAddres):
+    print("Changing MAC address for " + interface + " to " + NewMacAddres)
+    subprocess.call("ifconfig " + interface + " down", shell=True)
+    subprocess.call("ifconfig " + interface + " hw ether " +  NewMacAddres, shell=True)
+    subprocess.call("ifconfig " + interface + " up", shell=True)
+
+SetNewAddressMac(interface,NewMacAddres)
