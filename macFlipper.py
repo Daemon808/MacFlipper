fcf6cf144f57b0fa52a109acc977651f654fbd96
Hugo <Hugoramand@gmail.com>
2024-08-21 18:17:03 +0200

    change to snake_case + verify MAC address

--- macFlipper.py
+++ macFlipper.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
 
+import re
 import subprocess
 import optparse
 
-def getArguments():
-    parser = optparse.OptionParser()
-    parser.add("-i","--interface",dest="interface",help="Interface to changer MAC address")
-    parser.add("-m","--mac",dest="newMacAddress" ,help="New MAC Address")
-    (option, arguments) = parser.parse_args()
-    if not parser.interface:
-        parser.error("Please specify interface, use --help for more information")
-    elif not parser.newMacAddress:
-            parser.error("Please specify MAC address, use --help for more information")
-    return option
+def search_mac_address(string):
+    return re.search(r'([0-9A-Fa-f]{2}[: -]){5}([0-9A-Fa-f]{2})', str(string)).group(0)
 
-def SetNewAddressMac(interface, NewMacAddres):
-    print("Changing MAC address for " + interface + " to " + NewMacAddres)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac_address", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac_address:
+        parser.error("[-] Please specify a new MAC address, use --help for more info.")
+    return options
+
+def set_new_mac_address(interface, new_mac_address):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
     subprocess.call("ifconfig " + interface + " down", shell=True)
-    subprocess.call("ifconfig " + interface + " hw ether " +  NewMacAddres, shell=True)
+    subprocess.call("ifconfig " + interface + " hw ether " + new_mac_address, shell=True)
     subprocess.call("ifconfig " + interface + " up", shell=True)
 
-option = getArguments()
-SetNewAddressMac(option.interface,option.newMacAddress)
+def get_mac_address_from_interface(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = search_mac_address(ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result
+    else:
+        print("[-] Could not read MAC address.")
+
+options = get_arguments()
+current_mac_address = get_mac_address_from_interface(options.interface)
+print("Current MAC address for interface " + options.interface + " is: " + current_mac_address)
